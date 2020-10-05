eedfb20d524f0e1dcb4a7847e07d14bc130bd249
Luis Altuve <luis2ra@gmail.com>
2020-10-05 13:08:43 -0400

    Nueva Practica usando react-hook-form: convertir un binario en decimal

---
+++ my-react-hook-form-2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EjemploDos from './components/EjemploDos'
+
+
+function App() {
+    return (
+        <div className="container mt-2">
+            <EjemploDos />           
+        </div>
+    );
+}
+
+export default App;
