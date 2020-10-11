546369d81eee72ddaf6286f2340385a1683d6840
Luis Altuve <luis2ra@gmail.com>
2020-10-10 21:56:13 -0400

    Crud App with Hooks

--- my-crud-1/src/App.js
+++ my-crud-1/src/App.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserTable from './components/UserTable';
+import { v4 as uuidv4 } from 'uuid';
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
 
+const App = () => {
 
-function App() {
+	// const usersData = []
+
+	const usersData = [
+		{ id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+		{ id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+		{ id: uuidv4(), name: 'Ben', username: 'benisphere' },
+	]
+
+	// state
+	const [users, setUsers] = useState(usersData);
+
+	// Add user
+	const addUser = (user) => {
+		user.id = uuidv4()
+		setUsers([
+			...users,
+			user
+		])
+	}
+
+	// Delete user
+	const deleteUser = (id) => {
+		// console.log(id)
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	// Edit user
+	const [editing, setEditing] = useState(false);
+	
+	const [currentUser, setCurrentUser] = useState({
+		id: null,
+		name: '',
+		username: ''
+	});
+
+	const editRow = (user) => {
+		setEditing(true);
+		setCurrentUser({
+			id: user.id,
+			name: user.name,
+			username: user.username
+		});
+	};
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false);
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
 
 	return (
-		<h1>Hola Mundo</h1>
-	);
+		<div className="container">
+			<h1>CRUD App with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{
+						editing ? (
+							<div>
+								<h2>Edit user</h2>
+								<EditUserForm 
+									currentUser={currentUser}
+									updateUser={updateUser}
+									/>
+							</div>
+						) : (
+							<div>
+								<h2>Add user</h2>
+								<AddUserForm addUser={addUser}/>
+							</div>
+						)
+					}
+				</div>
+				<div className="flex-large">
+					<h2>View users</h2>
+					<UserTable 
+						users={users} 
+						deleteUser={deleteUser} 
+						editRow={editRow}
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App;
